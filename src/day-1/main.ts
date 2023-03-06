24132631be3103e7f96387d0dd5134973e67ebb4
ogzhanolguncu <ogzhan11@gmail.com>
2023-03-07 00:45:09 +0300

    Finish second part of the day 1

--- src/day-1/main.ts
+++ src/day-1/main.ts
@@ -3,7 +3,9 @@ import fs from "fs/promises";
 const findTheMostCalorieCarryingElf = async () => {
   let startIndex = 0;
   const calorie = await fs.readFile("input.txt", "utf8");
-  const listOfCalcCalories = calorie.split("\r\n").reduce((acc, crr, index, arr) => {
+  const listOfCalcCalories = calorie
+    .split("\r\n")
+    .reduce((acc, crr, index, arr) => {
       if (crr === "") {
         acc.push(
           arr
@@ -11,12 +13,16 @@ const findTheMostCalorieCarryingElf = async () => {
             .reduce((acc, crr) => Number(acc) + Number(crr), 0)
         );
         startIndex = index + 1;
-    }
-    
-    return acc;
-  }, [] as number[]);
-  return Math.max(...listOfCalcCalories);
+      }
 
+      return acc;
+    }, [] as number[]);
+
+  const sortedByLowestToHighest = listOfCalcCalories.sort((a, b) => a - b);
+  const top = sortedByLowestToHighest.pop() ?? 0;
+  const second = sortedByLowestToHighest.pop() ?? 0;
+  const third = sortedByLowestToHighest.pop() ?? 0;
+  return top + second + third;
 };
 
 findTheMostCalorieCarryingElf();
